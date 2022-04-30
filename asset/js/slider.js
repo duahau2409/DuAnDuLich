08f0732b457db47cd006bc0cbae6c8d03de3d927
iruka2409 <luatbeo12@gmail.com>
2022-04-30 17:48:22 +0700

    DuAnDuLich

---
+++ asset/js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.image-slider').slick({
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 10000,
+        nextArrow: '.wwanext',
+        prevArrow: '.wwaprev',
+        dots: true
+        });
+    });
+      
